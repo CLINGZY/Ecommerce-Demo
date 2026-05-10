916b70e63b82a018b6ade914896311a3c5e94bf3
CLINTON <clintonuduophori@gmail.com>
2026-05-10 07:53:53 +0100

    changed images path on utils.js

--- JS/utils.js
+++ JS/utils.js
@@ -17,12 +17,12 @@ class Product {
 }
 
 
-const Product1 = new Product(1, "Phone", 250, "../images/phone.jpg", "In Stock")
-const Product2 = new Product(2, "Ear Pods", 100, "../images/ear-pods.jpg", "In Stock")
-const Product3 = new Product(3, "Laptop", 350, "../images/laptop.jpg", "In Stock")
-const Product4 = new Product(4, "Watch", 75, "../images/watch.jpg", "In Stock")
-const Product5 = new Product(5, "Shoe", 50, "../images/shoes.jpg", "In Stock")
-const Product6 = new Product(6, "Bag", 30, "../images/bag.jpg", "In Stock")
+const Product1 = new Product(1, "Phone", 250, "./images/phone.jpg", "In Stock")
+const Product2 = new Product(2, "Ear Pods", 100, "./images/ear-pods.jpg", "In Stock")
+const Product3 = new Product(3, "Laptop", 350, "./images/laptop.jpg", "In Stock")
+const Product4 = new Product(4, "Watch", 75, "./images/watch.jpg", "In Stock")
+const Product5 = new Product(5, "Shoe", 50, "./images/shoes.jpg", "In Stock")
+const Product6 = new Product(6, "Bag", 30, "./images/bag.jpg", "In Stock")
 
 export const products = [Product1, Product2, Product3, Product4, Product5, Product6]
 
